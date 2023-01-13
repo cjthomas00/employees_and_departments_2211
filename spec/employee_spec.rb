dd977aa61792692e081bec3c0e5a036766180c89
Caleb Thomas <cjthomas00@comcast.net>
2023-01-13 09:09:27 -0700

    test: created #initialize test.

---
+++ spec/employee_spec.rb
@@ -0,0 +1,14 @@
+require './lib/employee'
+
+RSpec.describe Employee do
+  describe 'Iteration #1' do
+    let(:bobbi) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})}
+
+    it 'exists and has attributes' do
+      expect(bobbi).to be_instance_of(Employee)
+      expect(bobbi.name).to eq("Bobbi Jaeger")
+      expect(bobbi.age).to eq(30)
+      expect(bobbi.salary).to eq(100000)
+    end
+  end
+end
