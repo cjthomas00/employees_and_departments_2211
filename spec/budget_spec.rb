a197234c1d989123e53dd07542acd018346eb98e
Caleb Thomas <cjthomas00@comcast.net>
2023-01-13 09:36:29 -0700

    test: create #initialize test.

---
+++ spec/budget_spec.rb
@@ -0,0 +1,14 @@
+require './lib/budget'
+require './lib/department'
+require './lib/employee'
+
+RSpec.describe Budget do
+  describe 'Iteration #3' do
+  let(:budget) { Budget.new("2023") }
+    
+    it 'exists and has attributes' do
+      expect(budget).to be_instance_of(Budget)
+      expect(budget.year).to eq("2023")
+    end
+  end 
+end
