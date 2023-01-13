0550b1bb19dd4d79dbf65fcdf398804047976237
Caleb Thomas <cjthomas00@comcast.net>
2023-01-13 09:10:03 -0700

    feat: #initialize created.

---
+++ lib/employee.rb
@@ -0,0 +1,11 @@
+class Employee
+  attr_reader :name, 
+              :age,
+              :salary
+  
+  def initialize(info)
+    @name = info[:name]
+    @age = info[:age].to_i
+    @salary = info[:salary].delete_prefix("$").to_i
+  end
+end
