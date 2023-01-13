9213135806c5591226b50137dcb0995b866dfaab
Caleb Thomas <cjthomas00@comcast.net>
2023-01-13 10:07:13 -0700

    test: create test for #employee_salaries

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe Budget do
   let(:budget) { Budget.new("2023") }
   let(:customer_service) { Department.new("Customer Service") }
   let(:finance) { Department.new("Finance") }
+  let(:bobbi) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"}) }
+  let(:aaron) { Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"}) }
+  let(:ricky) { Employee.new({name: "Ricky Bobby", age: "47", salary: "115000"}) }
+  let(:gertrude) { Employee.new({name: "Gertrude Jones", age: "63", salary: "145000"}) }
     
     it 'exists and has attributes' do
       expect(budget).to be_instance_of(Budget)
@@ -34,6 +38,18 @@ RSpec.describe Budget do
       
       finance.expense(250)
       expect(budget.expenses_under(500)).to eq([])
+    end
+
+    it 'can list #employee_salaries' do
+      budget.add_department(customer_service)
+      budget.add_department(finance)
+
+      customer_service.hire(bobbi)
+      customer_service.hire(aaron)
+      finance.hire(ricky)
+      finance.hire(gertrude)
+
+      expect(budget.employee_salaries).to eq([bobbi.salary, aaron.salary, ricky.salary, gertrude.salary])
     end
   end 
 end
