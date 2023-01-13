ee186d1b8491d4a28a41297bf42c1b08759f4f45
Caleb Thomas <cjthomas00@comcast.net>
2023-01-13 09:20:37 -0700

    test: created test for #hire.

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -4,6 +4,8 @@ require './lib/employee'
 RSpec.describe Department do 
   describe 'Iteration #2' do
   let(:customer_service) { Department.new("Customer Service") }
+  let(:bobbi) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"}) }
+  let(:aaron) { Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"}) }
 
     it 'exists and has attributes' do
       expect(customer_service).to be_instance_of(Department)
@@ -12,6 +14,12 @@ RSpec.describe Department do
 
     it 'starts without #employees' do
       expect(customer_service.employees).to eq([])
+    end
+
+    it 'can #hire employees' do
+      customer_service.hire(bobbi)
+      customer_service.hire(aaron)
+      expect(customer_service.employees).to eq([bobbi, aaron])
     end
   end
 end
